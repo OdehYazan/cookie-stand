eb8693eb0877f1f118b99c9f044057519c404ce7
OdehYazan <eng.yazanodeh@gmail.com>
2021-05-05 14:03:33 +0300

    lab075/5

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-let tableEl = document.getElementById('table');
+let tableEl = document.getElementById('createTable');
 let hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
 let cookiesLocations = [];
 function Sales(shopName, minHourlyCustomers, maxHourlyCustomers, avgCookie) {
